cc6b419e48bde0ac9dc534d1b70a5db3b9af8727
Praveen-Rangavajhula <praveen.rangavajhula@gmail.com>
2023-11-11 10:09:14 -0500

    Initial Commit

---
+++ src/main/java/com/db/project/ugadining/UgaDiningCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.db.project.ugadining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgaDiningCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgaDiningCommonsApplication.class, args);
+	}
+
+}
